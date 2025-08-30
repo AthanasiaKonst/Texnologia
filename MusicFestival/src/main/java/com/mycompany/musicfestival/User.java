6eb377306db914a018c5433f5c512032d5a170fe
marilia <118003982+marilia22222@users.noreply.github.com>
2025-08-30 22:22:13 +0300

    Delete MusicFestival directory

--- MusicFestival/src/main/java/com/mycompany/musicfestival/User.java
+++
@@ -1,7 +0,0 @@
-//o user exei mono duo xaraktiristika thn login kai thn logout
-package main.java.com.mycompany.musicfestival;
-public interface User {
-    void login();
-    void logout();
-    
-}
--- MusicFestival/src/main/java/com/mycompany/musicfestival/organiser/OrganiserActions.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.mycompany.musicfestival.organiser;
-
-import java.util.List;
-
-public interface OrganiserActions {
-    void createFestival(String name, String location, String date);
-    void acceptArtist(String artistName);
-     void viewArtistApplications(List<String> artistApplications);
-}
